c05dde1d6321be9e9cd7e319ce34bc7fca5555d7
sakamoto <sakamoto@securesky-tech.com>
2020-03-05 23:43:49 +0900

    add snacks.mypkgdemo1.num_op and its test code

---
+++ snacks/mypkgdemo1/num_op.py
@@ -0,0 +1,14 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def sub(left, right):
+    return left - right
+
+
+def mul(left, right):
+    return left * right
+
+
+def div(left, right):
+    return left / right
