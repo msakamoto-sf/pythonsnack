abfac39035357e04a99b71ca234a83cd5012d9df
sakamoto <sakamoto@securesky-tech.com>
2020-03-13 15:33:35 +0900

    add typing demo test code

--- tests/snacks/typing/test_typing_demo.py
+++ tests/snacks/typing/test_typing_demo.py
@@ -1,18 +1,15 @@
 import unittest
 from dataclasses import dataclass, field
-from typing import List, Dict
+from typing import List, Dict, TypeVar, Generic
 
 # demonstration of typing module.
 
 
-# https://docs.python.org/ja/3/library/dataclasses.html
 @dataclass
-class MyTypeDemos:
+class MyTypeDemo1:
     str1: str = "hello typing module"
     int1: int = 100
 
-    # see: https://stackoverflow.com/questions/53632152/why-cant-dataclasses-have-mutable-defaults-in-their-class-attributes-declaratio # noqa: E501
-    # see: https://stackoverflow.com/questions/52063759/passing-default-list-argument-to-dataclasses # noqa: E501
     strings: List[str] = field(default_factory=lambda: ["aaa", "bbb", "ccc"])
     ints: List[int] = field(default_factory=lambda: [10, 20, 30])
     str2int: Dict[str, int] = field(
@@ -27,12 +24,9 @@ class MyTypeDemos:
     )
 
 
-# TODO more and more typing combination demo
-
-
-class TestTypingDemo(unittest.TestCase):
-    def test_my_type_demo(self):
-        a: MyTypeDemos = MyTypeDemos()
+class TestMyTypeDemo1(unittest.TestCase):
+    def test_do(self):
+        a: MyTypeDemo1 = MyTypeDemo1()
         self.assertEqual(a.str1, "hello typing module")
         self.assertEqual(a.int1, 100)
         self.assertListEqual(a.strings, ["aaa", "bbb", "ccc"])
@@ -46,3 +40,64 @@ class TestTypingDemo(unittest.TestCase):
         self.assertListEqual(a.str2strings["aaa"], ["AA", "BB", "CC",])
         self.assertListEqual(a.str2strings["bbb"], ["DD", "EE",])
         self.assertListEqual(a.str2strings["ccc"], [])
+
+
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
+T4 = TypeVar("T4")
+
+
+class MyTypeDemoPair1(Generic[T1, T2]):
+    left: T1
+    right: T2
+
+    def __init__(self, left: T1, right: T2):
+        self.left = left
+        self.right = right
+
+
+class TestMyTypeDemoPair1(unittest.TestCase):
+    def test_do(self):
+        p1: MyTypeDemoPair1[int, str] = MyTypeDemoPair1(100, "hello")
+        self.assertEqual(p1.left, 100)
+        self.assertEqual(p1.right, "hello")
+        p2: MyTypeDemoPair1[str, int] = MyTypeDemoPair1("world", 200)
+        self.assertEqual(p2.left, "world")
+        self.assertEqual(p2.right, 200)
+
+
+class MyTypeDemoPair2(Generic[T1, T2, T3], MyTypeDemoPair1[T1, T2]):
+    middle: T3
+
+    def __init__(self, left: T1, middle: T3, right: T2):
+        super().__init__(left, right)
+        self.middle = middle
+
+
+class TestMyTypeDemoPair2(unittest.TestCase):
+    def test_do(self):
+        p1: MyTypeDemoPair2[int, str, int] = MyTypeDemoPair2(100, "hello", 200)
+        self.assertEqual(p1.left, 100)
+        self.assertEqual(p1.middle, "hello")
+        self.assertEqual(p1.right, 200)
+
+
+class MyTypeDemoPair3(Generic[T4], MyTypeDemoPair1[str, str]):
+    middle: T4
+
+    def __init__(self, left: str, middle: T4, right: str):
+        super().__init__(left, right)
+        self.middle = middle
+
+
+class TestMyTypeDemoPair3(unittest.TestCase):
+    def test_do(self):
+        p1: MyTypeDemoPair3[int] = MyTypeDemoPair3("hello", 100, "world")
+        self.assertEqual(p1.left, "hello")
+        self.assertEqual(p1.middle, 100)
+        self.assertEqual(p1.right, "world")
+
+
+# TODO more and more typing combination demo
+
