6377058d7488c4a9235277f6cf5bef3373a4017d
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2020-03-15 23:59:32 +0900

    add test_multiprocessing_demo.py(WIP, TODO)

---
+++ tests/snacks/test_multiprocessing_demo.py
@@ -0,0 +1,7 @@
+# TODO
+# demonstration fo multiprocess
+# ref: https://docs.python.org/ja/3/library/multiprocessing.html
+# ref: https://docs.python.org/ja/3/library/threading.html
+# see-also:
+# https://qiita.com/hidetobara/items/11618e1709b0a4f21e65
+# https://docs.python.org/ja/3/glossary.html#term-global-interpreter-lock
