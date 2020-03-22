f46b4fd2c85d588a68231c98535cae2e0a8d0ed9
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2020-03-22 23:54:01 +0900

    add test_static_method_inheritance_demo()

--- tests/snacks/test_class_inheritance_demo.py
+++ tests/snacks/test_class_inheritance_demo.py
@@ -15,6 +15,14 @@ class TestClassInheritanceDemo(TestCase):
             def get_attr1(self):
                 return self.attr1
 
+            def get_attrx(self):
+                # get_attrz() is not defined in C1
+                return self.get_attry() + self.get_attrz() + self.attr1
+
+            def get_attry(self):
+                # default implementation
+                return 0
+
         class C2a(C1):
             class_attr1 = 200
 
@@ -27,6 +35,13 @@ class TestClassInheritanceDemo(TestCase):
 
             def get_attr2(self):
                 return self.attr2
+
+            def get_attry(self):
+                # override
+                return self.attr2 * 2
+
+            def get_attrz(self):
+                return self.attr2 - 2
 
         class C2b(C1):
             class_attr1 = 300
@@ -42,6 +57,13 @@ class TestClassInheritanceDemo(TestCase):
             def get_attr2(self):
                 return self.attr2
 
+            def get_attry(self):
+                # override
+                return self.attr2 * 3
+
+            def get_attrz(self):
+                return self.attr2 - 3
+
         self.assertEqual(C1.class_attr1, 100)
         self.assertEqual(C2a.class_attr1, 200)
         self.assertEqual(C2b.class_attr1, 300)
@@ -53,11 +75,15 @@ class TestClassInheritanceDemo(TestCase):
         self.assertEqual(o2a.attr2, 21)
         self.assertEqual(o2a.get_attr1(), 20)
         self.assertEqual(o2a.get_attr2(), 21)
+        # 21(=attr2) * 2 + 21(=attr2) - 2 + 20(=attr1) : C1#get_attrx() calls C2a#get_attrz()
+        self.assertEqual(o2a.get_attrx(), 81)
         o2b = C2b(30)
         self.assertEqual(o2b.attr1, 30)
         self.assertEqual(o2b.attr2, 32)
         self.assertEqual(o2b.get_attr1(), 30)
         self.assertEqual(o2b.get_attr2(), 32)
+        # 32(=attr2) * 3 + 32(=attr2) - 3 + 30(=attr1) : C1#get_attrx() calls C2b#get_attrz()
+        self.assertEqual(o2b.get_attrx(), 155)
         self.assertTrue(isinstance(o1, C1))
         self.assertTrue(isinstance(o2a, C1))
         self.assertTrue(isinstance(o2a, C2a))
@@ -72,15 +98,84 @@ class TestClassInheritanceDemo(TestCase):
         self.assertTrue(issubclass(C2b, C1))
         self.assertFalse(issubclass(C2a, C2b))
 
-    def test_template_method_demo(self):
-        # TODO
-        pass
-
     def test_static_method_inheritance_demo(self):
-        # TODO
-        pass
+        # see-also:
+        # https://docs.python.org/ja/3/library/functions.html#super
+        # https://stackoverflow.com/questions/26788214/super-and-staticmethod-interaction/26807879
+        # https://stackoverflow.com/questions/805066/call-a-parent-classs-method-from-child-class
+        class C1:
+            m1 = 100
+
+            @classmethod
+            def add_c(cls, a):
+                return cls.m1 + a
+
+            @staticmethod
+            def add_s(a, b):
+                return a + b
+
+            @classmethod
+            def sub_c(cls, a):
+                return cls.m1 - a
+
+            @staticmethod
+            def sub_s(a, b):
+                return a - b
+
+        class C2a(C1):
+            @classmethod
+            def add_c2(cls, a):
+                return C1.add_c(a) + 1
+
+            @staticmethod
+            def add_s2(a, b):
+                return C1.add_s(a, b) + 2
+
+            @classmethod
+            def sub_c(cls, a):
+                # override
+                return C1.sub_c(a) - 1
+
+            @staticmethod
+            def sub_s(a, b):
+                # override
+                return C1.sub_s(a, b) - 2
+
+        class C2b(C1):
+            @classmethod
+            def add_c2(cls, a):
+                return super(C2b, cls).add_c(a) + 3
+
+            @staticmethod
+            def add_s2(a, b):
+                return super(C2b, C2b).add_s(a, b) + 4
+
+            @classmethod
+            def sub_c(cls, a):
+                # override
+                return super(C2b, cls).sub_c(a) - 3
+
+            @staticmethod
+            def sub_s(a, b):
+                # override
+                return super(C2b, C2b).sub_s(a, b) - 4
+
+        self.assertEqual(C2a.add_c(10), 110)  # C1.m1 + 10
+        self.assertEqual(C2a.add_c2(10), 111)  # C1.m1 + 10 + 1
+        self.assertEqual(C2a.add_s2(10, 20), 32)  # 10 + 20 + 2
+        self.assertEqual(C2a.sub_c(10), 89)  # C1.m1 - 10 - 1
+        self.assertEqual(C2a.sub_s(10, 20), -12)  # 10 - 20 - 2
+        self.assertEqual(C2b.add_c(20), 120)  # C1.m1 + 20
+        self.assertEqual(C2b.add_c2(20), 123)  # C1.m1 + 20 + 3
+        self.assertEqual(C2b.add_s2(20, 30), 54)  # 20 + 30 + 4
+        self.assertEqual(C2b.sub_c(20), 77)  # C1.m1 - 20 - 3
+        self.assertEqual(C2b.sub_s(20, 30), -14)  # 20 - 30 - 4
 
     def test_multiple_inheritance_demo(self):
+        # TODO
+        pass
+
+    def test_method_overload_demo(self):
         # TODO
         pass
 
