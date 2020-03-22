bc6568df390657f50bf566ac6037dcd335550280
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2020-03-23 00:34:11 +0900

    add test_multiple_inheritance_demo()

--- tests/snacks/test_class_inheritance_demo.py
+++ tests/snacks/test_class_inheritance_demo.py
@@ -172,10 +172,67 @@ class TestClassInheritanceDemo(TestCase):
         self.assertEqual(C2b.sub_s(20, 30), -14)  # 20 - 30 - 4
 
     def test_multiple_inheritance_demo(self):
-        # TODO
-        pass
+        # see-also
+        # https://docs.python.org/ja/3/tutorial/classes.html#multiple-inheritance
+        # https://docs.python.org/ja/3/library/functions.html#super
+        # https://qiita.com/Kodaira_/items/42dfe18c81af98bf0db3
+        # https://stackoverflow.com/questions/3277367/how-does-pythons-super-work-with-multiple-inheritance
+        class C1:
+            def __init__(self, f1):
+                self.f1 = f1
+
+            def add(self, a):
+                # print("C1", self.f1)
+                return self.f1 + a
+
+            def sub(self, a):
+                return self.f1 - a
+
+        class C2a(C1):
+            def __init__(self, f1):
+                super().__init__(f1 + 1)
+                self.f2 = f1 + 2
+
+            def add(self, a):
+                x = super().add(a)
+                # print("C2a", x)
+                return x + self.f2 + 3
+
+            def mul(self, a):
+                return self.f1 * self.f2 * a
+
+        class C2b(C1):
+            def __init__(self, f1):
+                super().__init__(f1 + 4)
+                self.f2 = f1 + 5
+
+            def add(self, a):
+                x = super().add(a)
+                # print("C2b", x)
+                return x + self.f2 + 6
+
+            def div(self, a):
+                return self.f1 / a + self.f2
+
+        class C3(C2a, C2b):
+            def __init__(self, f1):
+                super().__init__(f1)
+
+        o3 = C3(10)
+        self.assertEqual(o3.f1, 15)  # C2a#__init__() and C2b#__init__()
+        self.assertEqual(o3.f2, 12)  # C2a#__init__()
+        self.assertEqual(o3.add(30), 78)  # = C2a#add(C2b#add(C1#add()))
+        # C1#add() = 15 + 30 => 45
+        # C2b#add() = 45 + 12 + 6 => 63
+        # C2a#add() = 63 + 12 + 3 => 78
+        self.assertEqual(o3.sub(10), 5)  # 15 - 10
+        self.assertEqual(o3.mul(2), 360)  # 15 * 12 * 2
+        self.assertEqual(o3.div(3), 17)  # 15/3 + 12
 
     def test_method_overload_demo(self):
+        # see-also:
+        # https://stackoverflow.com/questions/10202938/how-do-i-use-method-overloading-in-python
+        # https://docs.python.org/ja/3/library/functools.html#functools.singledispatch
         # TODO
         pass
 
